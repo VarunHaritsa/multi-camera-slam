37cb5de2e3dfb58d0e61aedb3ea20649dce7f612
Varun Haritsa <varun.haritsa96@gmail.com>
2023-09-04 01:14:38 -0400

    Camera Interface running

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,30 @@
 #include "FeatureExtractor.hh"
-#include "BaseStructs.hh"
+#include "CameraInterface.hh"
 
 #include <image_transport/image_transport.h>
 #include <sensor_msgs/Image.h>
 
-int main (int argc, char **argv)
+int main(int argc, char **argv)
 {
-    return 0;
+    // Initialize ros node
+    ros::init(argc, argv, "VisualSlamNode");
+    ros::NodeHandle nh;
+    ROS_INFO_STREAM("Successfully initialized visual slam node");
+    
+    auto camera = slam::CameraInterface();
+    auto orb = slam::VisualFeatureExtractor();
+    camera.Initialize(slam::CameraModeEnum::LIVE_CAMERA, nh);
+
+    ros::Rate loopRate(10);
+    while (ros::ok())
+    {
+        // Publish the image onto the ros topic
+        camera.Run();
+
+        
+        ros::spinOnce();
+        loopRate.sleep();
+    }
+
+    return EXIT_SUCCESS;
 }
